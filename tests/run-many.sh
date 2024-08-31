c09e3d0c269957267cda422d6fc81f09127c4faa
Luna Wang <lunawang257@gmail.com>
2024-08-31 08:04:03 -0700

    better output; move threads to inner loop

--- tests/run-many.sh
+++ tests/run-many.sh
@@ -15,16 +15,18 @@ prog="../build/tests/tlx_container_btree_speedtest_concurrent"
 
 rm -f /tmp/out
 
-for ((thread=1;thread<=$MAX_THREAD;thread++)); do
+for rootSlot in 1 -2; do
     for lockReq in all root no-root none ; do
-	for rootSlot in 1 -2; do
-	    printf '\r%02d:%02d: ' "$(( SECONDS/60 ))" "$(( SECONDS%60 ))"
-	    echo -ne "$prog -r 64 -m $N -M $N -i 0 -l 100 -s -t $thread -R $rootSlot -L $lockReq > /tmp/one-out"
+	for ((thread=1;thread<=$MAX_THREAD;thread++)); do
+	    printf '%02d:%02d: ' "$(( SECONDS/60 ))" "$(( SECONDS%60 ))"
+	    echo "$prog -r 64 -m $N -M $N -i 0 -l 100 -s -t $thread -R $rootSlot -L $lockReq > /tmp/one-out"
 	    if [ "$dryrun" != "1" ] ; then
 		$prog -r 64 -m $N -M $N -i 0 -l 100 -s -t $thread -R $rootSlot -L $lockReq > /tmp/one-out
 		if [ ! -f "/tmp/out" ]; then
+		    tail -2 /tmp/one-out
 		    tail -2 /tmp/one-out > /tmp/out
 		else
+		    tail -1 /tmp/one-out
 		    tail -1 /tmp/one-out >> /tmp/out
 		fi
 	    fi
