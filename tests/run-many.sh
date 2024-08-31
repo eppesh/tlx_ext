c6b14aa2405580bae53785f4e02f216dc3a23817
Luna Wang <lunawang257@gmail.com>
2024-08-30 22:43:57 -0700

    add run-many.sh to run many tests

---
+++ tests/run-many.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ "$1" == "-n" ]; then
+    dryrun=1
+else
+    dryrun=0
+fi
+
+# smaller will reduce run time
+REPEAT=64
+MAX_THREAD=6
+N=1024000
+
+prog="../build/tests/tlx_container_btree_speedtest_concurrent"
+
+rm -f /tmp/out
+
+for ((thread=1;thread<=$MAX_THREAD;thread++)); do
+    for lockReq in all root no-root none ; do
+	for rootSlot in 1 -2; do
+	    echo $prog -r 64 -m $N -M $N -i 0 -l 100 -s -t $thread -R $rootSlot -L $lockReq
+	    if [ "$dryrun" != "1" ] ; then
+		$prog -r 64 -m $N -M $N -i 0 -l 100 -s -t $thread -R $rootSlot -L $lockReq > /tmp/one-out
+		if [ ! -f "/tmp/out" ]; then
+		    tail -2 /tmp/one-out > /tmp/out
+		else
+		    tail -1 /tmp/one-out >> /tmp/out
+		fi
+	    fi
+	done
+    done
+done
